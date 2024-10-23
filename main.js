1650401d0553744f20b9c5436012738f61ab1d20
Akshansh <akshanshsingh00@gmail.com>
2024-10-23 08:37:36 +0530

    Finished

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-// you can import the module in your main.js file
-// eg: import Vue from 'vue' or import { createApp } from 'vue'
+const addButton = document.getElementById("add-button");
+addButton.addEventListener("click", async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/users");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const { users } = data;
+    const checkAge = (item) => {
+      if (item.age >= 30) {
+        console.log(`${item.firstName} ${item.lastName}`);
+      }
+    };
+    users.forEach(checkAge);
+    window.alert("Users with age over 30 have been logged in the console!");
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
